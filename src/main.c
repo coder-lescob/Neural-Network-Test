464bba24ccf529a1a3f965f91dd8e1ca7c3990b0
coder-lescob <gabriel.lescob@outlook.com>
2026-01-07 16:49:52 +0100

    importing iris data set and starting figure out that it's almost a csv just with the header

--- src/main.c
+++ src/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <genann.h>
 
+char *speciesNames[] = {"Iris-setosa", "Iris-versicolor", "Iris-virginica"};
+
 int main(void) {
-    printf("hello, world !\n");
+    printf("%s\n", speciesNames[0]);
     return 0;
 }
